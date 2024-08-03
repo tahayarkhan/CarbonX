986bfac911d4c2c2c528a6ea1b34ba44ce3e6330
Taha Khan <tahakhan@Tahas-MacBook-Air.local>
2024-08-03 19:17:57 -0400

    setup the backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, authUser } = require('../controllers/userController..js');
+const { registerUser, authUser, getUser } = require('../controllers/userController.js');
 
 router.route('/').post(registerUser);
 router.route('/login').post(authUser);
-
+router.route('/').get(getUser);
 module.exports = router;
